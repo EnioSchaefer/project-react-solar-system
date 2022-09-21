f6370920916564c50625c58dd209e55b83c73f7c
Enio Schaefer <enio100schaefer@hotmail.com>
2022-09-21 13:50:56 -0300

    requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <span data-testid="planet-card">{planetName}</span>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+};
+
+PlanetCard.defaultProps = {
+  planetName: 'jooj2',
+  planetImage: 'jooj3',
+};
+
+export default PlanetCard;
